a6ab725a8ce9d2ac8d12cff98d3931cea3416126
Talladego <nico.sjolund@gmail.com>
2025-08-13 19:45:10 +0200

    chore(ui): show app version badge next to heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import RoRview from './RoRview';
+import pkg from '../package.json';
 
 function App() {
   return (
     <>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 16, margin: '8px 0 0 0' }}>
-        <h1 style={{ color: '#eee', textAlign: 'center', margin: 0 }}>RORplanner</h1>
+        <h1 style={{ color: '#eee', textAlign: 'center', margin: 0 }}>
+          RORplanner <span className="app-version" title={`Version ${pkg.version}`}>v{pkg.version}</span>
+        </h1>
       </div>
       <RoRview />
     </>
